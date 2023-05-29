034d26d18358b646cc478265b05b0ab7da0c8738
FluuppyPup0121 <98412730+FluuppyPup0121@users.noreply.github.com>
2023-05-29 15:07:51 +0200

    Update twojamatka.sql
    Widzowie mamy toooooo

--- twojamatka.sql
+++ twojamatka.sql
@@ -1,51 +1,62 @@
-Uzytkownicy {
-	id integer pk increments
-	Imie text
-	Nazwisko text
-	Nazwa_Uzytkownika text
-	Haslo text
-	Id_Zamowienia integer >* Zamowienia.id
-	Adres integer *>* Adresy.id
-	Formy_Platnosci integer >* Formy_Platnosci.id
-	Administrator boolean
-}
+DROP DATABASE IF EXISTS foodEx;
+CREATE DATABASE foodEx;
+USE foodEx;
 
-Zamowienia {
-	id integer pk increments
-	id_dan integer >* Dania_w_restauracji_XD.id_dwr
-	Cena float
-	Adres integer *>* Adresy.id
-}
+CREATE TABLE Uzytkownicy (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    Imie TEXT(250),
+    Nazwisko TEXT(250),
+    Nazwa_Uzytkownika TEXT(250),
+    Haslo TEXT(250),
+    Id_Zamowienia INT(11),
+    Adres INT(11),
+    Formy_Platnosci INT(11),
+    Administrator BOOL,
+    FOREIGN KEY (Id_Zamowienia) REFERENCES Zamowienia(id),
+    FOREIGN KEY (Adres) REFERENCES Adresy(id),
+    FOREIGN KEY (Formy_Platnosci) REFERENCES Formy_Platnosci(id)
+);
 
-Restauracje {
-	id integer pk increments
-	nazwa_restauracji text
-	Miasto text
-}
+CREATE TABLE Zamowienia (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    id_dan INT(11),
+    Cena FLOAT(20),
+    Adres INT(11),
+    FOREIGN KEY (id_dan) REFERENCES Dania_w_restauracji_XD(id_dwr),
+    FOREIGN KEY (Adres) REFERENCES Adresy(id)
+);
 
-Dania {
-	id integer pk increments
-	Nazwa text
-	Cena float
-}
+CREATE TABLE Restauracje (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    nazwa_restauracji TEXT(250),
+    Miasto TEXT(250)
+);
 
-Adresy {
-	id integer pk increments
-	Miasto text
-	Ulica text
-	Nr_Domu/Mieszkania integer
-	Kod_Pocztowy integer
-}
+CREATE TABLE Dania (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    Nazwa TEXT(250),
+    Cena FLOAT(20)
+);
 
-Formy_Platnosci {
-	id integer pk increments
-	Karta integer
-	Paypal integer
-	W_Naturze boolean
-}
+CREATE TABLE Adresy (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    Miasto TEXT(250),
+    Ulica TEXT(250),
+    Nr_Domu_Mieszkania INT(11),
+    Kod_Pocztowy INT(11)
+);
 
-Dania_w_restauracji_XD {
-	id_dwr integer pk increments
-	id_res integer *> Restauracje.id
-	id_Dania integer *>* Dania.id
-}
+CREATE TABLE Formy_Platnosci (
+    id INT(11) PRIMARY KEY AUTO_INCREMENT,
+    Karta INT(11),
+    Paypal INT(11),
+    W_Naturze BOOL
+);
+
+CREATE TABLE Dania_w_restauracji_XD (
+    id_dwr INT(11) PRIMARY KEY AUTO_INCREMENT,
+    id_res INT(11),
+    id_Dania INT(11),
+    FOREIGN KEY (id_res) REFERENCES Restauracje(id),
+    FOREIGN KEY (id_Dania) REFERENCES Dania(id)
+);
