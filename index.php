bb48db5ceed9c19b0e598ebb7498b7714f328bb8
Tomasz Glogowski <tomaszglogowski044@gmail.com>
2023-06-11 18:18:43 +0200

    Create index.php. Change location nav.php.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html lang="pl">
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <title>Jedzenie</title>
+</head>
+
+<body>
+<?php
+require 'nav.php';
+?>
+
+</body>
+</html>
