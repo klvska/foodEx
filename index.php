e11e1cd65a23e53c0c683dea99fb9f8d0d3d5953
Jakub Sierant <90271504+klvska@users.noreply.github.com>
2023-06-11 20:20:38 +0200

    Styled index.php

--- index.php
+++ index.php
@@ -4,12 +4,24 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
     <title>Jedzenie</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
 <body>
 <?php
 require 'nav.php';
 ?>
+    <div class="container">
+    <div class="text">
+      <h1>Najszybsza dostawa, najszybsza obsługa</h1>
+      <p>Oferujemy najszybszą dostawę oraz najwyższą jakośc obsługi klienta na rynku. Ty zamawiasz, a my dostarczamy.</p>
+      <div class="button-container">
+    <a href="products.php" class="button">Zamów</a>
+  </div>
+    </div>
+    <div class="image"></div>
+  </div>
+  
 
 </body>
 </html>
