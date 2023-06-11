cb29f29fb38784522924eb5dfea8f8f3978b4630
Jakub Sierant <90271504+klvska@users.noreply.github.com>
2023-06-12 00:54:52 +0200

    Update login.php

--- logowanie/login.php
+++ logowanie/login.php
@@ -18,43 +18,49 @@ if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true))
     <title>Jedzenie</title>
     <style>
     @import url('https://fonts.googleapis.com/css2?family=Ubuntu&display=swap');
-    *{
-        font-family: 'Ubuntu', sans-serif;
-        font-style: normal;
-        font-weight: 700;
-    }
-    a {
+
+* {
+  font-family: 'Ubuntu', sans-serif;
+  font-style: normal;
+  font-weight: 700;
+}
+
+a {
   text-decoration: none;
 }
 
-
 #modal {
-  background: #F7F7F7;;
+  background: #F7F7F7;
   border-radius: 42px;
   box-shadow: 20px 20px 34px 5px rgba(0, 0, 0, 0.25);
-  height: 643px;
   margin: 6rem auto 8.1rem auto;
   width: 690px;
+  height: 730px;
 }
+
 #modal-content {
   padding: 12px 84px;
 }
+
 #modal-title {
-  font-size: 54px;   
+  font-size: 4rem;
   letter-spacing: 2px;
-  padding-bottom: 23px;
-  padding-top: 13px;
+  padding-bottom: 1.5rem;
+  padding-top: 0.75rem;
   text-align: center;
 }
+
 #signup {
   color: black;
-  font-size: 24px;
-  margin-top: 50px;
+  font-size: 2rem;
+  margin-top: 3rem;
   text-align: center;
 }
-span{
-    color:  #F07F00;
+
+span {
+  color: #F07F00;
 }
+
 #submit-btn {
   background: #F07F00;
   border: none;
@@ -62,34 +68,79 @@ span{
   box-shadow: 0px 1px 8px #a61e50;
   cursor: pointer;
   color: white;
-  height: 86px;
+  height: 4.5rem;
   margin: 0 auto;
-  margin-top: 50px;
+  margin-top: 3rem;
   transition: 0.25s;
-  width: 339px;
-  font-size: 36px;
+  width: 80%;
+  font-size: 2rem;
 }
+
 #submit-btn:hover {
   box-shadow: 0px 1px 18px #F07F00;
 }
+
 .form {
-  align-items: left;
   display: flex;
   flex-direction: column;
 }
+
 .form-border {
   background: #F07F00;
   height: 1px;
   width: 100%;
 }
+
 .form-content {
-  background: #F7F7F7;;
+  background: #F7F7F7;
   border: none;
   outline: none;
   padding-top: 14px;
+  font-size: 2rem;
 }
-input{
-    font-size: 24px;
+
+input {
+  font-size: 2rem;
+}
+
+@media screen and (max-width: 768px) {
+  #modal {
+    max-width: 500px;
+  }
+
+  #modal-title {
+    font-size: 3rem;
+  }
+
+  #submit-btn {
+    height: 4rem;
+    font-size: 1.8rem;
+  }
+
+  .form-content,
+  input {
+    font-size: 1.8rem;
+  }
+}
+
+@media screen and (max-width: 480px) {
+  #modal {
+    max-width: 320px;
+  }
+
+  #modal-title {
+    font-size: 2.5rem;
+  }
+
+  #submit-btn {
+    height: 3.5rem;
+    font-size: 1.6rem;
+  }
+
+  .form-content,
+  input {
+    font-size: 1.6rem;
+  }
 }
 
     
@@ -98,7 +149,7 @@ input{
 
 <body>
     <?php
-    require 'nav.php';
+    require '../nav.php';
     ?>
 <div id="modal">
     <div id="modal-content">
